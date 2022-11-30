77427678627e0756ac8ac30492ef6d5804008188
User <User@DESKTOP-FPDF7SA>
2022-11-30 23:54:09 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/handler/TopMenuHandler.java
+++ codingjoa/src/main/java/com/codingjoa/handler/TopMenuHandler.java
@@ -9,9 +9,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import com.codingjoa.entity.Category;
 import com.codingjoa.service.CategoryService;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @ControllerAdvice(basePackages = "com.codingjoa.controller")
 public class TopMenuHandler {
 
@@ -20,13 +17,7 @@ public class TopMenuHandler {
 	
 	@ModelAttribute("parentCategory")
 	public List<Category> findParentCategory() {
-		log.info("============== TopMenuHandler ==============");
-		
-		List<Category> list = categoryService.findParentCategory();
-		log.info("{}", list);
-		
-		return list;
-		//return categoryService.findParentCategory();
+		return categoryService.findParentCategory();
 	}
 	
 }
