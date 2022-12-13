6f5b63fae919ad8099bdda61c138c13b1b368179
User <User@DESKTOP-FPDF7SA>
2022-12-13 15:50:08 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/validator/UploadFileValidator.java
+++ codingjoa/src/main/java/com/codingjoa/validator/UploadFileValidator.java
@@ -17,7 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 @Component(value = "uploadFileValidator")
 public class UploadFileValidator implements Validator {
 
-	private static long MAX_FILE_SIZE = 20 * 1024 * 1024;
+	private static long MAX_FILE_SIZE = 10 * 1024 * 1024;
 	
 	@Override
 	public boolean supports(Class<?> clazz) {
