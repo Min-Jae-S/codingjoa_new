86eb8bb50e5215e7d1ff09472f942f4a295f7dea
User <User@DESKTOP-FPDF7SA>
2023-01-03 17:30:53 +0900

    commit

---
+++ codingjoa/src/main/java/com/codingjoa/handler/ClobTypeHandler.java
@@ -0,0 +1,39 @@
+package com.codingjoa.handler;
+
+import java.io.StringReader;
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.TypeHandler;
+
+// MyBatis-3.0.3-User-Guide.pdf, 17/64
+// getCLOB not implemented for class oracle.jdbc.driver.T4CLongAccessor
+public class ClobTypeHandler implements TypeHandler<Object> {
+	
+	@Override
+	public void setParameter(PreparedStatement ps, int i, Object parameter, JdbcType jdbcType) 
+			throws SQLException {
+		String s = (String) parameter;
+        StringReader reader = new StringReader(s);
+        ps.setCharacterStream(i, reader, s.length());
+	}
+
+	@Override
+	public Object getResult(ResultSet rs, String columnName) throws SQLException {
+		return rs.getString(columnName);
+	}
+
+	@Override
+	public Object getResult(ResultSet rs, int columnIndex) throws SQLException {
+		return rs.getString(columnIndex);
+	}
+
+	@Override
+	public Object getResult(CallableStatement cs, int columnIndex) throws SQLException {
+		return cs.getString(columnIndex);
+	}
+
+}
