e3dca3350ff8c33ca627ad9afe39fd9f19103c80
User <User@DESKTOP-FPDF7SA>
2023-03-03 16:20:28 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/security/service/CustomAuthenticationEntryPoint.java
+++ codingjoa/src/main/java/com/codingjoa/security/service/CustomAuthenticationEntryPoint.java
@@ -10,6 +10,8 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+import com.codingjoa.error.ErrorResponse;
+
 import lombok.extern.slf4j.Slf4j;
 
 /*	
@@ -26,14 +28,38 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+	private final String DEFAULT_FAILURE_URL = "/member/login";
+	
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException, ServletException {
 		log.info("============== CustomAuthenticationEntryPoint ==============");
-		log.info("[default] response.getContentType()={}", response.getContentType());
 		
-		// 401(Unauthorized) vs 403(Forbidden), https://mangkyu.tistory.com/146
-		//response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		/*	# ajax 요청 확인 
+		 	https://0taeng.tistory.com/30
+		 	https://mohwaproject.tistory.com/entry/Ajax-%EC%A0%84%EC%86%A1-%EA%B5%AC%EB%B6%84%ED%95%98%EA%B8%B0
+		
+		 	1. header: x-requested-with(key) --> XMLHttpRequest(value) 
+				- x: Non-standard
+				- jquery나 대중성 있는 라이브러리들이 ajax전송시 기본으로 추가하여 전송
+		
+		 	2. custom header
+				beforeSend: function(xmlHttpRequest) {
+					xmlHttpRequest.setRequestHeader("AJAX", "true")
+				}
+				...
+		*/
+		
+		boolean ajax = "XMLHttpRequest".equals(request.getHeader("x-requested-with")) ? true : false;
+		log.info("ajax={}", ajax);
+		
+		if (ajax) {
+			// 401(Unauthorized) vs 403(Forbidden)
+			// https://mangkyu.tistory.com/146
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		} else {
+			request.getRequestDispatcher(DEFAULT_FAILURE_URL).forward(request, response);
+		}
 		
 	}
 
