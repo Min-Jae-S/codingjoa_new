9e0599b290c52bb12484d336bd992ed68e1fd202
MinJae <MinJae@DESKTOP-BB7TVT9>
2023-04-12 19:41:13 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/validator/BoardCategoryCodeValidator.java
+++ codingjoa/src/main/java/com/codingjoa/validator/BoardCategoryCodeValidator.java
@@ -7,26 +7,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.codingjoa.annotation.BoardCategoryCode;
-import com.codingjoa.mapper.CategoryMapper;
 import com.codingjoa.service.CategoryService;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component(value = "boardCategoryCodeValidator")
-public class BoardCategoryCodeValidator implements ConstraintValidator<BoardCategoryCode, Integer> {
+public class BoardCategoryCodeValidator implements ConstraintValidator<BoardCategoryCode, Object> {
 	
 	@Autowired
 	private CategoryService categoryService;
 	
 	@Override
-	public boolean isValid(Integer value, ConstraintValidatorContext context) {
+	public boolean isValid(Object value, ConstraintValidatorContext context) {
 		log.info("============== BoardCategoryCodeValidator ==============");
+		log.info("{}", categoryService);
 		
-		if (!categoryService.isBoardCategory(value)) {
-			return false;
+		boolean result = true;
+		
+		if (value instanceof String) {
+			String rawBoardCategoryCode = String.valueOf(value);
+			try {
+				int boardCategoryCode = Integer.parseInt(rawBoardCategoryCode);
+				if (!categoryService.isBoardCategory(boardCategoryCode)) {
+					result = false;
+				}
+			} catch (NumberFormatException e) {
+				result = false;
+			}
 		}
 		
-		return true;
+		return result;
 	}
 }
