4eaa3a2d426882a7b66ba8895c845335df13c9d8
MinJae <MinJae@DESKTOP-BB7TVT9>
2023-04-12 19:05:42 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/validator/BoardCategoryCodeValidator.java
+++ codingjoa/src/main/java/com/codingjoa/validator/BoardCategoryCodeValidator.java
@@ -3,21 +3,30 @@ package com.codingjoa.validator;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.codingjoa.annotation.BoardCategoryCode;
+import com.codingjoa.mapper.CategoryMapper;
+import com.codingjoa.service.CategoryService;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component(value = "boardCategoryCodeValidator")
-public class BoardCategoryCodeValidator implements ConstraintValidator<BoardCategoryCode, Object> {
+public class BoardCategoryCodeValidator implements ConstraintValidator<BoardCategoryCode, Integer> {
+	
+	@Autowired
+	private CategoryService categoryService;
 	
 	@Override
-	public boolean isValid(Object value, ConstraintValidatorContext context) {
+	public boolean isValid(Integer value, ConstraintValidatorContext context) {
 		log.info("============== BoardCategoryCodeValidator ==============");
+		
+		if (!categoryService.isBoardCategory(value)) {
+			return false;
+		}
 		
 		return true;
 	}
-	
 }
