dfee8fe1eccdb98a4d47a4aaf17c69e5497bbaac
MinJae <MinJae@DESKTOP-BB7TVT9>
2023-06-04 15:34:23 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/test/TestConverter.java
+++ codingjoa/src/main/java/com/codingjoa/test/TestConverter.java
@@ -5,12 +5,13 @@ import org.springframework.core.convert.converter.Converter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class TestConverter implements Converter<String, Test> {
+public class TestConverter implements Converter<String, Integer> {
 
 	@Override
-	public Test convert(String source) {
+	public Integer convert(String source) {
 		log.info("-------- {} --------", this.getClass().getSimpleName());
 		log.info("\t > source = {}", source);
 		return null;
 	}
+	
 }
