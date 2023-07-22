905f9aa5847b756970ba9916472daed6f67aaf8b
User <User@DESKTOP-2P7H5OO>
2023-07-22 14:48:35 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/test/TestAnnoProcessor.java
+++ codingjoa/src/main/java/com/codingjoa/test/TestAnnoProcessor.java
@@ -5,32 +5,42 @@ import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SupportedAnnotationTypes({
+	"com.codingjoa.annotation.CodeCallRequired",
+    "com.codingjoa.annotation.MessageAlreadySet"
+})
 public class TestAnnoProcessor extends AbstractProcessor {
 
 	@Override
 	public synchronized void init(ProcessingEnvironment processingEnv) {
-		// TODO Auto-generated method stub
+		log.info("## {} : init", this.getClass().getSimpleName());
 		super.init(processingEnv);
 	}
 
 	@Override
 	public Set<String> getSupportedAnnotationTypes() {
-		// TODO Auto-generated method stub
+		log.info("## {} : getSupportedAnnotationTypes", this.getClass().getSimpleName());
+		log.info("\t > {}", super.getSupportedAnnotationTypes());
 		return super.getSupportedAnnotationTypes();
 	}
 
 	@Override
 	public SourceVersion getSupportedSourceVersion() {
-		// TODO Auto-generated method stub
+		log.info("## {} : getSupportedSourceVersion", this.getClass().getSimpleName());
+		log.info("\t > {}", SourceVersion.latestSupported());
 		return super.getSupportedSourceVersion();
 	}
 
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-		// TODO Auto-generated method stub
+		log.info("## {} : process", this.getClass().getSimpleName());
 		return false;
 	}
 
