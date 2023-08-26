414443469d87d9aa463fc4a69def3fe13dcc031b
User <User@DESKTOP-2P7H5OO>
2023-08-26 23:08:37 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/controller/TestSchedulerController.java
+++ codingjoa/src/main/java/com/codingjoa/controller/TestSchedulerController.java
@@ -31,7 +31,6 @@ public class TestSchedulerController {
 	 * 	Timer has only one execution thread; ScheduledThreadPoolExecutor can be configured with any number of threads.
 	 * 	Runtime Exceptions thrown inside the TimerTask kill the thread, so the following scheduled tasks won't run further; 
 	 * 	with ScheduledThreadExecutor, the current task will be cancelled, but the rest will continue to run.
-	 * 
 	 */
 	
 	private final Timer timer = new Timer();
