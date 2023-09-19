054f8c4da57e65d08ba1c7d926b4c777df81a1ff
User <User@DESKTOP-2P7H5OO>
2023-09-19 20:09:56 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/scheduler/JobC.java
+++ codingjoa/src/main/java/com/codingjoa/scheduler/JobC.java
@@ -1,7 +1,12 @@
 package com.codingjoa.scheduler;
 
+import javax.annotation.Resource;
+
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,16 +23,24 @@ public class JobC extends QuartzJobBean {
 	@Autowired
 	private JobExplorer jobExplorer;
 	
+	@Resource(name = "batchJob")
+	private Job job;
+	
 	@Override
 	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
 		log.info("## {}", this.getClass().getSimpleName());
+		log.info("\t > jobLauncher = {}", jobLauncher);
+		log.info("\t > jobExplorer = {}", jobExplorer);
+		//log.info("\t > batch jobs = {}", jobExplorer.getJobNames());
+		
 		try {
-			log.info("\t > jobLauncher = {}", jobLauncher);
-			log.info("\t > jobExplorer = {}", jobExplorer);
-			//log.info("\t > batch jobs = {}", jobExplorer.getJobNames());
+			JobParameters jobParameters = new JobParametersBuilder(this.jobExplorer)
+					.getNextJobParameters(this.job)
+					.toJobParameters();
+			log.info("\t > jobParameters = {}", jobParameters);
+			jobLauncher.run(job, jobParameters);
 		} catch (Exception e) {
-			log.info("\t > exception = {}", e.getClass().getSimpleName());
-			log.info("\t > {}", e.getMessage());
+			log.info("\t > {} : {}", e.getClass().getSimpleName(), e.getMessage());
 		}
 	}
 }
