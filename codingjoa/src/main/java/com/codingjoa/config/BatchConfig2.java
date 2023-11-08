2cb19092fe7270c964ca2acae1611e833039a89b
User <User@DESKTOP-2P7H5OO>
2023-11-08 23:30:29 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/config/BatchConfig2.java
+++ codingjoa/src/main/java/com/codingjoa/config/BatchConfig2.java
@@ -4,6 +4,8 @@ import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
@@ -38,6 +40,24 @@ public class BatchConfig2 extends DefaultBatchConfigurer {
 	public void setDataSource(@Qualifier("batchDataSource") DataSource dataSource) {
 		super.setDataSource(dataSource);
 	}
-	
+
+	@Override
+	protected JobRepository createJobRepository() throws Exception {
+		JobRepositoryFactoryBean factory = new JobRepositoryFactoryBean();
+		factory.setDataSource(dataSource);
+		factory.setTransactionManager(transactionManager);
+		factory.setDatabaseType("H2");
+	    factory.setTablePrefix("BATCH_");
+	    
+	    // ORA-08177: can't serialize access for this transaction
+	    // The issue arises when multiple Spring Batch jobs share a single JobRepository and are executed concurrently. 
+	    // This occurs because, by default, the IsolationLevelForCreate property of the transactionManager is set to ISOLATION_SERIALIZABLE 
+	    // when not specified separately.
+	    factory.setIsolationLevelForCreate("ISOLATION_DEFAULT");
+	    factory.afterPropertiesSet();
+	    JobRepository jobRepository = factory.getObject();
+	    log.info("## jobRepository" + System.lineSeparator() + "\t\t\t\t\t\t\t > {}", jobRepository);
+		return jobRepository;
+	}
 	
 }
