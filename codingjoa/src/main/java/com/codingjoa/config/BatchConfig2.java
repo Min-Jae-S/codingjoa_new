342e9bad93e6a93f7e2a12f25a45c4b4dc5da3a8
User <User@DESKTOP-2P7H5OO>
2023-11-13 22:38:26 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/config/BatchConfig2.java
+++ codingjoa/src/main/java/com/codingjoa/config/BatchConfig2.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+//@RequiredArgsConstructor
 @Configuration
 public class BatchConfig2 extends DefaultBatchConfigurer {
 	
