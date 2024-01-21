31e55a198d141a45bf3ed474485c37cafbaf79db
User <User@DESKTOP-2P7H5OO>
2024-01-21 23:41:10 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/service/test/TestTxIsoService.java
+++ codingjoa/src/main/java/com/codingjoa/service/test/TestTxIsoService.java
@@ -99,7 +99,7 @@ public class TestTxIsoService {
 	@Transactional(isolation = Isolation.READ_COMMITTED)
 	public void isoReadCommitted() {
 		log.info("## Isolation.READ_COMMITTED");
-		//checkTrasnaction();
+		checkTrasnaction();
 		
 		Integer firstCurrentNumber = isoMapper.findCurrentNumber();
 		log.info("\t > (1) current number = {} [ {} ]", firstCurrentNumber, Thread.currentThread().getName());
@@ -125,7 +125,7 @@ public class TestTxIsoService {
 	@Transactional(isolation = Isolation.SERIALIZABLE)
 	public void isoSerializable() {
 		log.info("## Isolation.SERIALIZABLE");
-		//checkTrasnaction();
+		checkTrasnaction();
 		
 		Integer firstCurrentNumber = isoMapper.findCurrentNumber();
 		log.info("\t > (1) current number = {} [ {} ]", firstCurrentNumber, Thread.currentThread().getName());
