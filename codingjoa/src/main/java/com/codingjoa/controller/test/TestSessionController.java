0a249af29a19f6a9ae98855799eeb42ab190db72
User <User@DESKTOP-2P7H5OO>
2024-04-16 20:32:42 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/controller/test/TestSessionController.java
+++ codingjoa/src/main/java/com/codingjoa/controller/test/TestSessionController.java
@@ -1,5 +1,7 @@
 package com.codingjoa.controller.test;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 
@@ -22,12 +24,13 @@ public class TestSessionController {
 		log.info("## test1");
 		log.info("\t > sessionId = {}", session.getId());
 		
+		long creationTime = session.getCreationTime();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		log.info("\t > created at {}", sdf.format(creationTime));
+		
 		List<String> attributeNames = Collections.list(session.getAttributeNames());
 		if (attributeNames.size() > 0) {
-			attributeNames.forEach(attributeName -> {
-				log.info("\t > {} : {}", attributeName, 
-						session.getAttribute(attributeName).getClass().getSimpleName());
-			});
+			log.info("\t > attributesNames = {}", attributeNames);
 		} else {
 			log.info("\t > no attributes");
 		}
