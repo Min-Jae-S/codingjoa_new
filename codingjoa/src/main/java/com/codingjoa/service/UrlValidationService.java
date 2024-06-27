634219785e0d7987f4e097c966ef0f161d112761
User <User@DESKTOP-2P7H5OO>
2024-06-27 22:33:34 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/service/UrlValidationService.java
+++
@@ -1,9 +0,0 @@
-package com.codingjoa.service;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface UrlValidationService {
-	
-	boolean validateUrl(HttpServletRequest request, String url);
-	
-}
