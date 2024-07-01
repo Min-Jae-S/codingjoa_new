d3801f41e409852ef368ba87fd2ab9c0465fb64d
User <User@DESKTOP-2P7H5OO>
2024-07-01 15:15:31 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/security/filter/RestAuthenticationFilter.java
+++ codingjoa/src/main/java/com/codingjoa/security/filter/RestAuthenticationFilter.java
@@ -66,7 +66,7 @@ public class RestAuthenticationFilter extends AbstractAuthenticationProcessingFi
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
 			Authentication authResult) throws IOException, ServletException {
 		log.info("## {}.successfulAuthentication", this.getClass().getSimpleName());
-		log.info("\t > authentication success. Updating SecurityContextHolder to contain: {}", authResult);
+		log.info("\t > authentication success. updating SecurityContextHolder to contain: {}", authResult.getClass().getSimpleName());
 		super.successfulAuthentication(request, response, chain, authResult);
 	}
 	
