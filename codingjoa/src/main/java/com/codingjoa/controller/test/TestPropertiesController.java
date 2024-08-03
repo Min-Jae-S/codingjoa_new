4d22f12554dbd3ca1fdc896ae17baa916c042a75
User <User@DESKTOP-2P7H5OO>
2024-08-03 18:39:19 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/controller/test/TestPropertiesController.java
+++ codingjoa/src/main/java/com/codingjoa/controller/test/TestPropertiesController.java
@@ -1,5 +1,7 @@
 package com.codingjoa.controller.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +16,16 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class TestPropertiesController {
 	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping("/test1")
 	public ResponseEntity<Object> test1() throws Exception {
 		log.info("## test1");
+		
+		String param1 = env.getProperty("security.param1");
+		log.info("\t > param1 = {}", param1);
+		
 		return ResponseEntity.ok(SuccessResponse.builder().message("success").build());
 	}
 
