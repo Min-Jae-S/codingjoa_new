e4252ddd7091cfc865e0023782fe5dbc4828354a
User <User@DESKTOP-2P7H5OO>
2024-10-18 23:01:24 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/aop/ExceptionHandlerAspect.java
+++ codingjoa/src/main/java/com/codingjoa/aop/ExceptionHandlerAspect.java
@@ -1,6 +1,9 @@
 package com.codingjoa.aop;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +14,29 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class ExceptionHandlerAspect {
 	
-	public void routeExceptionHandler(ProceedingJoinPoint joinPoint) {
+	@Around("execution(* com.example..*(..)) && @within(org.springframework.web.bind.annotation.ControllerAdvice)")
+	public Object routeExceptionHandler(ProceedingJoinPoint joinPoint) {
 		log.info("## {}.routeExceptionHandler", this.getClass().getSimpleName());
+		HttpServletRequest request = null;
+		for (Object arg : joinPoint.getArgs()) {
+			if (arg instanceof HttpServletRequest) {
+				request = (HttpServletRequest) arg;
+				break;
+			}
+		}
+		
+		if (request != null) {
+			if (isAjaxRequest(request)) {
+				log.info("\t > handling exception via ExceptionRestHandler");
+			} else {
+				log.info("\t > handling exception via ExceptionMvcHandler");
+			}
+		}
+		
+		return null;
+	}
+	
+	private boolean isAjaxRequest(HttpServletRequest request) {
+		return "XMLHttpRequest".equals(request.getHeader("x-requested-with"));
 	}
 }
