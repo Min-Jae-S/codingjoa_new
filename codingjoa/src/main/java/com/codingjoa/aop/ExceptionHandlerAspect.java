e73ea796fddad86a43b2d0e8502643d1364d1a97
User <User@DESKTOP-2P7H5OO>
2024-10-18 23:46:11 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/aop/ExceptionHandlerAspect.java
+++ codingjoa/src/main/java/com/codingjoa/aop/ExceptionHandlerAspect.java
@@ -15,21 +15,27 @@ import lombok.extern.slf4j.Slf4j;
 public class ExceptionHandlerAspect {
 	
 	@Around("execution(* com.example..*(..)) && @within(org.springframework.web.bind.annotation.ControllerAdvice)")
-	public Object routeExceptionHandler(ProceedingJoinPoint joinPoint) {
+	public Object routeExceptionHandler(ProceedingJoinPoint joinPoint) throws Throwable {
 		log.info("## {}.routeExceptionHandler", this.getClass().getSimpleName());
+		
 		HttpServletRequest request = null;
 		for (Object arg : joinPoint.getArgs()) {
+			log.info("\t > arg = {}", arg);
 			if (arg instanceof HttpServletRequest) {
 				request = (HttpServletRequest) arg;
 				break;
 			}
 		}
 		
+		log.info("\t > request = {}", request);
+		
 		if (request != null) {
 			if (isAjaxRequest(request)) {
 				log.info("\t > handling exception via ExceptionRestHandler");
+				return joinPoint.proceed();
 			} else {
 				log.info("\t > handling exception via ExceptionMvcHandler");
+				return joinPoint.proceed();
 			}
 		}
 		
