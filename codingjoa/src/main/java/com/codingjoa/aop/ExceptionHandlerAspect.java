8460057db0c66f64cdbd02d5e59e537645ae077f
User <User@DESKTOP-2P7H5OO>
2024-10-30 14:01:15 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/aop/ExceptionHandlerAspect.java
+++ codingjoa/src/main/java/com/codingjoa/aop/ExceptionHandlerAspect.java
@@ -1,19 +1,34 @@
 package com.codingjoa.aop;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.handler.AbstractHandlerMethodExceptionResolver;
+import org.springframework.web.servlet.mvc.method.annotation.ServletInvocableHandlerMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import com.codingjoa.util.AjaxUtils;
 
@@ -25,7 +40,7 @@ import lombok.extern.slf4j.Slf4j;
 @Aspect
 @RequiredArgsConstructor
 @Component
-public class ExceptionResolverAspect {
+public class ExceptionHandlerAspect {
 	
 	//private final ApplicationContext context;
 	private final WebApplicationContext context;
@@ -37,59 +52,46 @@ public class ExceptionResolverAspect {
 			+ "@within(org.springframework.web.bind.annotation.RestControllerAdvice)")
 	public void withinControllerAdviceAnnotations() {}
 
-	@Pointcut("@annotation(org.springframework.web.bind.annotation.ExceptionHandler)")
-	public void annotationExceptionHandler() {}
-
-	@Pointcut("execution(* org.springframework.web.servlet.HandlerExceptionResolver.resolveException(..))")
-	public void resolveExceptionByHandlerExceptionResolver() {}
-
-	@Pointcut("execution(* org.springframework.web.servlet.handler.HandlerExceptionResolverComposite.resolveException(..))")
-	public void resolveExceptionByHandlerExceptionResolverComposite() {}
-
-	@Pointcut("execution(* org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver.resolveException(..))")
-	public void resolveExceptionByExceptionHandlerExceptionResolver() {}
+	@Pointcut("execution(* org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver.doResolveHandlerMethodException(..))")
+	public void resolveExceptionHandlerPointcut() {}
 	
 	/*
-	public class HandlerExceptionResolverComposite implements HandlerExceptionResolver, Ordered {
-		
-		@Nullable
-		private List<HandlerExceptionResolver> resolvers;
-		private int order = Ordered.LOWEST_PRECEDENCE;
+	public class ExceptionHandlerExceptionResolver extends AbstractHandlerMethodExceptionResolver
+		implements ApplicationContextAware, InitializingBean {
 		
 		@Override
 		@Nullable
-		public ModelAndView resolveException(
-				HttpServletRequest request, HttpServletResponse response, @Nullable Object handler, Exception ex) {
-	
-			if (this.resolvers != null) {
-				for (HandlerExceptionResolver handlerExceptionResolver : this.resolvers) {
-					ModelAndView mav = handlerExceptionResolver.resolveException(request, response, handler, ex);
-					if (mav != null) {
-						return mav;
-					}
-				}
+		protected ModelAndView doResolveHandlerMethodException(HttpServletRequest request,
+				HttpServletResponse response, @Nullable HandlerMethod handlerMethod, Exception exception) {
+
+			ServletInvocableHandlerMethod exceptionHandlerMethod = getExceptionHandlerMethod(handlerMethod, exception);
+			if (exceptionHandlerMethod == null) {
+				return null;
 			}
-			return null;
+
+			...
 		}
 	}
 	*/
 	
-	//@Around("resolveExceptionByHandlerExceptionResolver()")
-	//@Around("resolveExceptionByHandlerExceptionResolverComposite()")
-	@Around("resolveExceptionByExceptionHandlerExceptionResolver()")
-	public Object resolveWithDynamicResolver(ProceedingJoinPoint joinPoint) throws Throwable {
-		log.info("## {}.resolveWithDynamicResolver", this.getClass().getSimpleName());
+	@Around("resolveExceptionHandlerPointcut()")
+	public Object arroundExceptionHanlderResolution(ProceedingJoinPoint joinPoint) throws Throwable {
+		log.info("## {}.arroundExceptionHanlderResolution", this.getClass().getSimpleName());
 		log.info("\t > target = {}", joinPoint.getTarget().getClass().getSimpleName());
 		
-		Object[] args = joinPoint.getArgs();
-		Object handler = args[2];
-		log.info("\t > handler = {}", handler);
-		
-		if (handler != null) {
-			return joinPoint.proceed();
+		HttpServletRequest request = null;
+		Object[] args = joinPoint.getArgs(); // HttpServletRequest, HttpServletResponse, HandlerMethod, Exception
+		for (int i = 0; i < args.length; i++) {
+			log.info("\t > arg[{}] = {}", i, args[i].getClass().getSimpleName());
+			if (args[i] instanceof HttpServletRequest) {
+				request = (HttpServletRequest) args[i];
+			}
 		}
 		
-		HttpServletRequest request = (HttpServletRequest) args[0];
+		if (request == null) {
+			log.info("\t > no HttpServletRequest instance in arguments");
+		}
+		
 		if (AjaxUtils.isAjaxRequest(request)) {
 			log.info("\t > ajax request detected, handling via ExceptionRestHandler");
 		} else {
