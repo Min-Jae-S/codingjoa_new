b6759f25e72e1bc0634959755ecfaf5512b0c83b
User <User@DESKTOP-2P7H5OO>
2024-12-05 12:49:11 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/config/WebSocketConfig.java
+++ codingjoa/src/main/java/com/codingjoa/config/WebSocketConfig.java
@@ -1,28 +1,29 @@
 package com.codingjoa.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+@ComponentScan("com.codingjoa.websocket")
+@EnableWebSocket
 @Configuration
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketConfigurer {
+	
+	private final WebSocketHandler webSocketHandler;
 
 	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		log.info("## registerStompEndpoints");
-		WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
-	}
-
-	@Override
-	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		log.info("## configureMessageBroker");
-		WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		log.info("## registerWebSocketHandlers");
+		registry.addHandler(webSocketHandler, "/ws")
+				.setAllowedOrigins("*");
 	}
 
 }
---
+++ codingjoa/src/main/java/com/codingjoa/websocket/WebSocketHandler.java
@@ -0,0 +1,27 @@
+package com.codingjoa.websocket;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+	
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		log.info("## {}.afterConnectionEstablished", this.getClass().getSimpleName());
+		super.afterConnectionEstablished(session);
+	}
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		log.info("## {}.afterConnectionClosed", this.getClass().getSimpleName());
+		super.afterConnectionClosed(session, status);
+	}
+
+	
+}
