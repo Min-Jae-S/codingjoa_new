7ba58ae287dcac27052a0702d23a8839566b9ce1
User <User@DESKTOP-2P7H5OO>
2024-12-18 23:59:45 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/websocket/test/StompChannelInterceptor.java
+++ codingjoa/src/main/java/com/codingjoa/websocket/test/StompChannelInterceptor.java
@@ -1,16 +1,22 @@
 package com.codingjoa.websocket.test;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class StompChannelInterceptor implements ChannelInterceptor {
+	
+	private final ObjectMapper objectMapper = new ObjectMapper();
 	
 	@Override
 	public boolean preReceive(MessageChannel channel) {
@@ -30,8 +36,14 @@ public class StompChannelInterceptor implements ChannelInterceptor {
 		Object payload = message.getPayload();
 		if (payload instanceof byte[]) {
 			byte[] bytes = (byte[]) payload;
-			String decoded = new String(bytes, StandardCharsets.UTF_8);
-			log.info("\t > payload = {}", decoded);
+			try {
+				log.info("\t > payload = {}", objectMapper.readValue(bytes, Map.class));
+			} catch (JsonParseException e) {
+				String decoded = new String(bytes, StandardCharsets.UTF_8);
+				log.info("\t > payload = {}", decoded);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 		
 		return message;
