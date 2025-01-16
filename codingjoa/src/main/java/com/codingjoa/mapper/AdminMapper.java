71d2c5ccb40ac39f62fe7b6568ede0e0cc8b473c
User <User@DESKTOP-2P7H5OO>
2025-01-16 23:56:07 +0900

    commit

---
+++ codingjoa/src/main/java/com/codingjoa/mapper/AdminMapper.java
@@ -0,0 +1,8 @@
+package com.codingjoa.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AdminMapper {
+	
+}
--- codingjoa/src/main/java/com/codingjoa/service/AdminService.java
+++ codingjoa/src/main/java/com/codingjoa/service/AdminService.java
@@ -1,52 +1,5 @@
 package com.codingjoa.service;
 
-import com.codingjoa.dto.AddrDto;
-import com.codingjoa.dto.AgreeDto;
-import com.codingjoa.dto.EmailAuthDto;
-import com.codingjoa.dto.JoinDto;
-import com.codingjoa.dto.MemberInfoDto;
-import com.codingjoa.dto.NicknameDto;
-import com.codingjoa.dto.PasswordChangeDto;
-import com.codingjoa.dto.PasswordSaveDto;
-import com.codingjoa.security.dto.PrincipalDetails;
-import com.codingjoa.security.oauth2.OAuth2Attributes;
-
 public interface AdminService {
 
-	void saveMember(JoinDto joinDto);
-	
-	void saveOAuth2Member(OAuth2Attributes oAuth2Attributes);
-	
-	void connectOAuth2Member(OAuth2Attributes oAuth2Attributes, Integer memberIdx);
-	
-	boolean isNicknameExist(String memberNickname);
-	
-	void checkEmailForJoin(String memeberEmail);
-	
-	void checkEmailForUpdate(String memberEmail, Integer memberIdx);
-	
-	void checkEmailForReset(String memberEmail);
-	
-	//String getMemberIdByEmail(String memberEmail);
-	
-	Integer getMemberIdxByIdAndEmail(String memberId, String memberEmail);
-	
-	void updateNickname(NicknameDto nicknameDto, Integer memberIdx);
-	
-	void updateEmail(EmailAuthDto emailAuthDto, Integer memberIdx);
-	
-	void updateAddr(AddrDto addrDto, Integer memberIdx);
-	
-	void updateAgree(AgreeDto agreeDto, Integer memberIdx);
-	
-	void updatePassword(PasswordChangeDto passwordChangeDto, Integer memberIdx);
-	
-	void savePassword(PasswordSaveDto passwordSaveDto, Integer memberIdx);
-	
-	MemberInfoDto getMemberInfoByIdx(Integer memberIdx);
-	
-	PrincipalDetails getUserDetailsByEmail(String memberEmail); // for authentication in UserDetailsService, OAuth2UserService
-
-	PrincipalDetails getUserDetailsByIdx(Integer memberIdx);	// for JWT re-issuance after updating
-	
 }
