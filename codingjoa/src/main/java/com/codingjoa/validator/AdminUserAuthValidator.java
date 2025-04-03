80167bb89949e72544d31136c13cadb9e09ee5ca
smj20228@naver.com <smj20228@naver.com>
2025-04-03 23:50:02 +0900

    commit

--- codingjoa/src/main/java/com/codingjoa/validator/AdminUserAuthValidator.java
+++ codingjoa/src/main/java/com/codingjoa/validator/AdminUserAuthValidator.java
@@ -1,20 +1,19 @@
 package com.codingjoa.validator;
 
-import java.util.regex.Pattern;
+import java.util.Set;
 
-import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import com.codingjoa.dto.AdminUserAuthDto;
-import com.codingjoa.dto.PasswordChangeDto;
 
 import lombok.extern.slf4j.Slf4j;
 
-@SuppressWarnings("unused")
 @Slf4j
 public class AdminUserAuthValidator implements Validator {
 
+	private static final Set<String> ROLES = Set.of("ROLE_USER", "ROLE_ADMIN");
+	
 	@Override
 	public boolean supports(Class<?> clazz) {
 		return AdminUserAuthDto.class.isAssignableFrom(clazz);
@@ -25,6 +24,18 @@ public class AdminUserAuthValidator implements Validator {
 		log.info("## {}", this.getClass().getSimpleName());
 
 		AdminUserAuthDto adminUserAuthDto = (AdminUserAuthDto) target;
+		Set<String> roles = adminUserAuthDto.getRoles();
+		
+		if (roles == null || roles.isEmpty()) {
+			errors.rejectValue("roles", "NotEmpty");
+		}
+		
+		for (String role : roles) {
+			if (!ROLES.contains(role)) {
+				errors.rejectValue("roles", "NotValid", new Object[] {role}, null);
+				return;
+			}
+		}
 	}
 
 }
